1aaeee1febb96053ca7a2a40f36deeead592b3ec
kai <ge62meq@mytum.de>
2025-06-04 16:31:33 +0200

    16:31 04/06/2025

---
+++ fr3_mujoco_ws/src/franka_mujoco_controller/launch/mujoco_controller_launch.py
@@ -0,0 +1,50 @@
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, TimerAction
+
+def launch():
+    """
+    Launch file that explicitly uses bash (not sh) and proper syntax
+    """
+   
+    return LaunchDescription([
+        # Start controller with explicit bash shell
+        TimerAction(
+            ## Add a time delay before starting the controller
+            period=2.0,
+            
+            ## add series of actions to be excuted
+            actions=[
+                ExecuteProcess(
+                    cmd=[
+                        '/bin/bash',
+                        '-c',
+                        'cd /media/kai/Kai_Backup/Master_Study/Master_Thesis/Master_Study_Master_Thesis && '
+                        'source setup_environment.sh && '
+                        'echo "Environment loaded successfully" && '
+                        'ros2 run franka_mujoco_controller mujoco_controller'
+                    ],
+                    name='mujoco_controller',
+                    output='screen'
+                    # Remove shell=True to avoid shell interpretation issues
+                )
+            ]
+        ),
+        
+        # Send start command
+        TimerAction(
+            period=5.0,
+            actions=[
+                ExecuteProcess(
+                    cmd=[
+                        'ros2', 'topic', 'pub', '--once',
+                        '/start_push', 'std_msgs/msg/String',
+                        
+                        ## This will start ROS2
+                        '{data: start}'  # Fixed quotes
+                    ],
+                    name='start_command',
+                    output='screen'
+                )
+            ]
+        )
+    ])
